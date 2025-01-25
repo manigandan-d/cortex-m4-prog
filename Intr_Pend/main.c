170311ae1081ad36159d029abc24e9c27ba485d4
manigandan-d <manigandan220916@gmail.com>
2025-01-25 12:00:54 +0530

    interrupt

--- Intr_Pend/main.c
+++ Intr_Pend/main.c
@@ -1,75 +1,55 @@
 #include <stdio.h>
 #include <stdint.h>
 
-#define __vo volatile
 
+#define __vo volatile
 #define ENABLE 1
 #define DISABLE 0
 
-#define NVIC_ISER0 ((__vo uint32_t *)0xE000E100)
 #define NVIC_ISER1 ((__vo uint32_t *)0xE000E104)
-#define NVIC_ISER2 ((__vo uint32_t *)0xE000E108)
-
-#define NVIC_ICER0 ((__vo uint32_t *)0XE000E180)
-#define NVIC_ICER1 ((__vo uint32_t *)0XE000E184)
-#define NVIC_ICER2 ((__vo uint32_t *)0XE000E188)
+#define NVIC_ICER1 ((__vo uint32_t *)0xE000E184)
+#define NVIC_ISPR1 ((__vo uint32_t *)0xE000E204)
 
 #define IRQ_NO_USART1 37
 #define IRQ_NO_USART2 38
 #define IRQ_NO_USART3 39
 
 void USART_IRQInterruptConfig(uint8_t IRQNumber, uint8_t EnorDi) {
-	if(EnorDi == ENABLE) {
-		if(IRQNumber <= 31) {
-			*NVIC_ISER0 |= (1 << IRQNumber);
-		}
-
-		else if(IRQNumber > 31 && IRQNumber < 64) {
-			*NVIC_ISER1 |= (1 << (IRQNumber % 32));
-		}
-
-		else if(IRQNumber >= 64 && IRQNumber < 96) {
-			*NVIC_ISER2 |= (1 << (IRQNumber % 64));
-		}
-	}
-
-	else {
-		if(IRQNumber <= 31) {
-			*NVIC_ICER0 |= (1 << IRQNumber);
-		}
-
-		else if(IRQNumber > 31 && IRQNumber < 64) {
-			*NVIC_ICER1 |= (1 << (IRQNumber % 32));
-		}
-		else if(IRQNumber >= 64 && IRQNumber < 96) {
-			*NVIC_ICER2 |= (1 << (IRQNumber % 64));
-		}
-	}
+    if (EnorDi == ENABLE) {
+        *NVIC_ISER1 |= (1 << (IRQNumber % 32));
+    } else {
+        *NVIC_ICER1 |= (1 << (IRQNumber % 32));
+    }
 }
 
 int main(void) {
-	uint32_t *pISPR1 = (uint32_t *)0xE000E204;
-	*pISPR1 |= (1 << (IRQ_NO_USART3 % 32));
+    // Manually pend USART1 interrupt
+    *NVIC_ISPR1 |= (1 << (IRQ_NO_USART1 % 32));
+    USART_IRQInterruptConfig(IRQ_NO_USART1, ENABLE);
+    USART_IRQInterruptConfig(IRQ_NO_USART1, DISABLE);
 
-	USART_IRQInterruptConfig(IRQ_NO_USART3, ENABLE);
+    // For USART2 testing
+    // *NVIC_ISPR1 |= (1 << (IRQ_NO_USART2 % 32));
+    // USART_IRQInterruptConfig(IRQ_NO_USART2, ENABLE);
+    // USART_IRQInterruptConfig(IRQ_NO_USART2, DISABLE);
 
-//	uint32_t *pISER1 = (uint32_t *)0xE000E104;
-//	*pISER1 |= (1 << (IRQ_NO_USART3 % 32));
+    // For USART3 testing
+    // *NVIC_ISPR1 |= (1 << (IRQ_NO_USART3 % 32));
+    // USART_IRQInterruptConfig(IRQ_NO_USART3, ENABLE);
+    // USART_IRQInterruptConfig(IRQ_NO_USART3, DISABLE);
 
-	USART_IRQInterruptConfig(IRQ_NO_USART3, DISABLE);
-
-	for(;;);
+    while (1); // Infinite loop
 }
 
+// USART ISRs
 void USART1_IRQHandler(void) {
-	printf("USART1 ISR\n");
+    printf("USART1 ISR executed\n");
 }
 
 void USART2_IRQHandler(void) {
-	printf("USART2 ISR\n");
+    printf("USART2 ISR executed\n");
 }
 
 void USART3_IRQHandler(void) {
-	printf("USART3 ISR\n");
+    printf("USART3 ISR executed\n");
 }
-
